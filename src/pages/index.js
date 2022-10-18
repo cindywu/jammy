e2eba2ed927ae609235505842b7f89b31ec4e7af
cindy <ciindy.wu@gmail.com>
2022-10-17 14:32:25 -1000

    add local mutations

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,15 @@ export default function Home() {
         licenseKey: 'l077f51b26f854f218b25a96e2e29b9eb',
         pushURL: '/api/replicache-push',
         pullURL: '/api/replicache-pull',
+        mutators: {
+          async createMessage(tx, {id, from, content, order}) {
+            await tx.put(`message/${id}`, {
+              from,
+              content,
+              order,
+            });
+          },
+        },
       });
     listen(rep);
     setRep(rep);
@@ -21,6 +30,8 @@ export default function Home() {
 
   return rep && <Chat rep={rep} />;
 }
+
+
 
 function Chat({rep}) {
   const messages = useSubscribe(
@@ -41,8 +52,16 @@ function Chat({rep}) {
 
   const onSubmit = e => {
     e.preventDefault();
-    // TODO: Create message
-  };
+    const last = messages.length && messages[messages.length -1][1]
+    const order = (last?.order ?? 0 ) + 1;
+    rep.mutate.createMessage({
+      id: nanoid(),
+      from: usernameRef.current.value,
+      content: contentRef.current.value,
+      order,
+    });
+    contentRef.current.value = '';
+  }
 
   return (
     <div className="flex flex-col p-4">
